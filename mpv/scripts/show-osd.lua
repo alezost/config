da1fe0849d19a10ade30f7d749b32e5621ad9bdf
Alex Kost <alezost@gmail.com>
2018-11-26 17:28:08 +0300

    mpv: Show progress on pause

--- mpv/scripts/show-osd.lua
+++ mpv/scripts/show-osd.lua
@@ -2,5 +2,10 @@ function al_show_osd (_event)
     mp.command("show-progress")
 end
 
+function al_osd_on_pause (_name, _value)
+    al_show_osd()
+end
+
 mp.register_event("file-loaded", al_show_osd)
 mp.register_event("seek", al_show_osd)
+mp.observe_property("pause", "bool", al_osd_on_pause)
