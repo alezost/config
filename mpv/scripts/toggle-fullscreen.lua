632860cd9d5a9486e018f08152099952a0f2fb04
Alex Kost <alezost@gmail.com>
2021-12-11 22:54:07 +0300

    mpv: Do not show OSD on "seek" and "pause"
    Show it on "play".

--- mpv/scripts/toggle-fullscreen.lua
+++ mpv/scripts/toggle-fullscreen.lua
@@ -4,6 +4,9 @@ function al_fs_on_pause (_name, value)
     if value == true then
         fs_state = mp.get_property("fullscreen")
         mp.set_property("fullscreen", "no")
+        -- For some reason, it shows progress only when we pause from a
+        -- fullscreen (maybe an mpv bug).
+        mp.command("show-progress")
     else
         mp.set_property("fullscreen", fs_state)
     end
