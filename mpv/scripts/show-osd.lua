24ecdeca963889b29288bbb097919ff2e118bf1b
Alex Kost <alezost@gmail.com>
2021-12-11 22:55:43 +0300

    mpv: Use "b" key to show time, file size, and file name

--- mpv/scripts/show-osd.lua
+++ mpv/scripts/show-osd.lua
@@ -1,5 +1,10 @@
 function al_show_osd (_event)
-    mp.command("show-progress")
+    mp.commandv("show-text",
+                mp.get_property_osd("duration")
+                .. " (" .. mp.get_property_osd("file-size") .. ")\n" ..
+                mp.get_property("filename/no-ext"),
+                4000)
 end
 
 mp.register_event("file-loaded", al_show_osd)
+mp.add_key_binding("b", al_show_osd)
